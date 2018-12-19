54757a78e7e0fc984d9dc5d75f4da9e69f0fd0c6
3ndrius <andrzejo1991@gmail.com>
2018-12-19 22:41:56 +0100

    Adding font-awesome
    Import localy font awesome 4.7 as css import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+//PACKAGE IMPORT
 import React, { Component } from 'react';
+
+//CSS IMPORTS
+import './font-awesome-4.7.0/css/font-awesome.min.css';
+
+// COMPONENT IMPORTS
 
 class App extends Component {
   render() {
