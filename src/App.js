9e7c653ab33ab208b048008e5375577f5d640c93
3ndrius <andrzejo1991@gmail.com>
2018-12-21 20:08:06 +0100

    Install styled component
    Adding styled component to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 //PACKAGE IMPORT
 import React, { Component } from 'react';
 
+
 //CSS IMPORTS
 import './font-awesome-4.7.0/css/font-awesome.min.css';
-
 // COMPONENT IMPORTS
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        work
+        <i className="fa fa-adn"></i>
+       
       </div>
     );
   }
