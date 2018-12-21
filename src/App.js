f3a55181eec73d9b36130254199732553f3b5a76
3ndrius <andrzejo1991@gmail.com>
2018-12-21 21:39:14 +0100

    File structure style components
    Adding file structure theme, components, layout, define utilt theme and
    colors for layout. Use theme provider for all component. Wrap all
    component  in Layout wrap component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 //PACKAGE IMPORT
 import React, { Component } from 'react';
-
+import styled from 'styled-components'
 
 //CSS IMPORTS
 import './font-awesome-4.7.0/css/font-awesome.min.css';
+import Layout from './layout/Layout';
+import Button from './components/buttons/Button';
 // COMPONENT IMPORTS
+
+
+const H1 = styled.h1 `
+  color:red;
+  font-size: ${(props) => props.isBig ? '5em' : '1em'}; 
+`;
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <i className="fa fa-adn"></i>
-       
-      </div>
+    <div className="App">
+    <Layout>
+      <H1 isBig={true}>Hello</H1>
+      <Button>Hello</Button>
+    </Layout>
+     
+     </div>
     );
   }
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,11 @@
+import {colors} from './colors';
+
+export const theme = {
+    colors,
+    fontWg: {
+        thin:300,
+        reg:400,
+        fat:800
+    }
+
+}
