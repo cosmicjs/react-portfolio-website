bfae8aefc45c4675592a885fccc51ff01e80b033
3ndrius <andrzejo1991@gmail.com>
2018-12-31 01:52:20 +0100

    Change media in contact component

--- src/components/page/Contact.js
+++ src/components/page/Contact.js
@@ -6,9 +6,16 @@ import Par from './../parts/Paragraph';
 
 
 const PageWrap = styled.div`
+${({theme}) => theme.media.mobile} {
   width:100%;
-  height:2000px;
+  height:100%;
+  padding:30px 0;
+  
+ }
+ ${({theme}) => theme.media.tablet} {
   padding:150px 0;
+ }
+  
 `;
 
 export default class Contact extends Component {
